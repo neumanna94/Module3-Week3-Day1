c81503d388a36d828ac55dcbf72e97884206867e
Guest User <Guest@epicodus-100.local>
2018-03-26 08:49:32 -0700

    initial set-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Task } from './models/task.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  tasks: Task[] = [
+    new Task('Task 1'),
+    new Task('Task2'),
+    new Task('Task3')
+  ];
 }
