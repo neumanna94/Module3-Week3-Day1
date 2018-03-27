ba51c58210926799e22baac50eb3d048dea0a034
Guest User <Guest@epicodus-103.local>
2018-03-27 12:42:04 -0700

    Practice seperation of components with root module and root components. Practice data up data down methodology.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,26 @@ import { Task } from './models/task.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  tasks: Task[] = [
-    new Task('Task 1'),
-    new Task('Task2'),
-    new Task('Task3')
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  selectedTask: Task = null;
+
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+
+  addTask(newTask: Task) {
+    this.masterTaskList.push(newTask);
+  }
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
 }
