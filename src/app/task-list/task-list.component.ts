3aecb83ece96733dfaec32540ad021d982f002e7
Guest User <Guest@epicodus-103.local>
2018-03-28 08:46:17 -0700

    Add pipes

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -9,10 +9,17 @@ import { Task } from '../models/task.model';
 export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
-
+  filterByCompleteness: string = "incompleteTasks";
   editButtonClicked(taskToEdit: Task) {
   this.clickSender.emit(taskToEdit);
 
+  }
+  onChange(optionFromMenu) {
+  this.filterByCompleteness = optionFromMenu;
+  }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+   clickedTask.done = setCompleteness;
   }
 
   priorityColor(currentTask){
